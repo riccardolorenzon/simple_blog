38bc2f2b07f53ed5d19f5cb3dee830de3db74751
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2014-12-14 19:10:05 +0100

    added groups and users API

--- simple_blog/settings.py
+++ simple_blog/settings.py
@@ -66,9 +66,8 @@ DATABASES = {
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGINATE_BY': 10
 }
 
 # Internationalization
--- simple_blog/urls.py
+++ simple_blog/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
 from blogapp import views
-from blogapp.views import UserViewSet
+from blogapp.views import UserViewSet, GroupViewSet
 from django.contrib import admin
 from rest_framework import routers, serializers, viewsets
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'', UserViewSet)
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -14,7 +15,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'sample_blog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', views.index, name = 'home'),
-    url(r'^api/users', include(router.urls)),
+    url(r'^api/', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^addblog/', views.createblog),
     url(r'^logout/', views.logout_view),
