e3fcc4c698b61a375f97715a25e0740ecbfc066a
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2014-12-14 18:31:58 +0100

    added users API

--- simple_blog/settings.py
+++ simple_blog/settings.py
@@ -63,6 +63,14 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
--- simple_blog/urls.py
+++ simple_blog/urls.py
@@ -1,6 +1,23 @@
 from django.conf.urls import patterns, include, url
 from blogapp import views
 from django.contrib import admin
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'', UserViewSet)
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -8,6 +25,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'sample_blog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', views.index, name = 'home'),
+    url(r'^api/users', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^addblog/', views.createblog),
     url(r'^logout/', views.logout_view),
