0c4f2e4ea309f00a7621d6d68f230892becaf945
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2014-12-14 19:01:59 +0100

    Added serializers

---
+++ blogapp/serializers.py
@@ -0,0 +1,14 @@
+__author__ = 'riccardo'
+from rest_framework import routers, serializers, viewsets
+from django.contrib.auth.models import User, Group
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('url', 'name')
+
--- blogapp/views.py
+++ blogapp/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from models import BlogArticle
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User, Group
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from rest_framework import viewsets
+from serializers import UserSerializer, GroupSerializer
 
 # index
 def index(request):
@@ -39,3 +42,17 @@ def createblog(request):
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect('/')
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
